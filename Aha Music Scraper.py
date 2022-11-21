557cb3574c2b6c450087ec8612758e8c637eef3d
Connor Richards <36263854+4lphabeta@users.noreply.github.com>
2022-11-21 01:15:04 +0000

    Update Aha Music Scraper.py
    Added csv to xlsx conversion feature.
    Added error trapping and a couple of QoL changes.

--- Aha Music Scraper.py
+++ Aha Music Scraper.py
@@ -1,17 +1,33 @@
-import time, datetime, webbrowser, openpyxl
+import time, datetime, webbrowser, openpyxl, sys
+import pandas as pd
 from pathlib import Path
-
-#Setting the user's download path
-downloads_path = str(Path.home() / "Downloads")
 
 #Setting the current date for file reading
 x = datetime.datetime.now()
 Date = x.strftime("%Y-%m-%d")
 
-#Load excel with its path
-data_folder = downloads_path + "\\aha-music-export_" + Date + ".xlsx"
-wb = openpyxl.load_workbook(data_folder)
+#Setting the user's download path and file path
+downloads_path = str(Path.home() / "Downloads")
+our_file = downloads_path + "\\aha-music-export_" + Date
+our_file_csv = Path(our_file + ".csv")
+our_file_xlsx = Path(our_file + ".xlsx")
 
+#Check if csv file exists
+if our_file_csv.is_file():
+    #Convert the csv to xlsx
+    print("Converting csv file to xlsx")
+    conv_file = pd.read_csv (our_file_csv)
+    conv_file.to_excel (our_file + ".xlsx", index = None, header=True)
+    print("File converted successfully")
+elif our_file_xlsx.is_file():
+    #An already converted file exists but there is no csv, continue as normal
+    print("xlsx file found despite finding no csv, continuing as normal")
+else:
+    print("csv file does not exist, please ensure that it is in your Downloads folder")
+    sys.exit()
+
+#Loading the Excel workbook
+wb = openpyxl.load_workbook(our_file + ".xlsx")
 ws = wb.active
 
 print("Workbook loaded")
@@ -20,9 +36,9 @@ print("Iterating through urls")
 #Iterate through rows and open each url
 for row in ws.iter_rows(min_row=2):
     url = row[5].value
-    webbrowser.open(url)
+    if url is not None:
+        webbrowser.open(url)
+        time.sleep(0.5)     #Delay slightly to hopefully prevent crashes
 
 print("All urls opened")
 print("Script finished")
-
-time.sleep(3)   #Sleep for 3 seconds
